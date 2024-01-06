b77d2d87101e32ed8bae5f301ed0bfd2bfe8b914
Marcin Bojara <marcin_5@mail.ru>
2024-01-06 18:44:28 +0100

    decorate messages repository as injectable

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessagesRepository } from './messages.repository';
 
 @Module({
   controllers: [MessagesController],
+  providers: [MessagesRepository],
 })
 export class MessagesModule {}
